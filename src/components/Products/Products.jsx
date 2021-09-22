7e3db55a6a2219044f4e6f5ae4b33994ec60b9ad
Korey Andrus <korey86@gmail.com>
2021-09-22 15:14:40 -0500

    add commerce.js and products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid';
+import { Grid } from '@material-ui/core';
 
 import Product from "./Product/Product";
 
+import useStyles from "./styles";
+
 const products = [
-    { id: 1, name: 'Shoes', description: "Running shoes.", price: '$5' },
-    { id: 2, name: 'Macbook', description: "Apple Macbook.", price: '$10'},
+    { id: 1, name: 'Shoes', description: "Running shoes.", price: '$5', image: "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHNob2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" },
+    { id: 2, name: 'Macbook', description: "Apple Macbook.", price: '$10', image: "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFjYm9va3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" },
 ];
 
-const Products = () => {
-    
+const Products = ({ products, onAddToCart }) => {
+    const classes = useStyles();
     
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart}/>
                     </Grid>
                 ))}
             </Grid>
