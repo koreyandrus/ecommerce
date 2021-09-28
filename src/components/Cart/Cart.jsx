67173a4e22ca4aa660680ecaee20ed7865e55365
Korey Andrus <korey86@gmail.com>
2021-09-28 14:52:25 -0500

    Update and remove items, start checkout component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
 
-const Cart = ( { cart }) => {
+const Cart = ( { cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
@@ -20,7 +20,7 @@ const Cart = ( { cart }) => {
                 {cart.line_items.map((item) => 
                 (
                         <Grid item xs={12} sm={4} key={item.id}>
-                            <CartItem item={item}></CartItem>
+                            <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}></CartItem>
                         </Grid>
                     ))}
             </Grid>
@@ -29,8 +29,8 @@ const Cart = ( { cart }) => {
                     Subtotal: { cart.subtotal.formatted_with_symbol }
                 </Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
-                    <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onCLick={handleEmptyCart}>Empty Cart</Button>
+                    <Button component={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
                 </div>
             </div>
         </>
