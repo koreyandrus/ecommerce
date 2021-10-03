0efa660e483073f8f9a7463954bae8084d66587f
Korey Andrus <korey86@gmail.com>
2021-10-03 08:59:39 -0500

    Change site name and link to live site

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # E-Commerce Website
 
 This is a personal project to show the use of the commerce.js api and also with Stripe added in for payments.
+
+Visit the live site now!
+
+(https://kacommerce.netlify.com "KACommerce")
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = ({ totalItems }) => {
               <Toolbar>
                 <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                     <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
-                    Custom Web Store
+                    KACommerce E-Store
                 </Typography>
                 <div className={classes.grow} />
                 {location.pathname === '/' && (
